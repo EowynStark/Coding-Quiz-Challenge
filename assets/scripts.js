481dfe69459e59e47049bb891ff2f87420e263fd
EowynStark <andwellm89@gmail.com>
2023-11-07 12:13:58 -0600

    creating script.js file

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+// create an initial function to begin the quiz
+
+// create a function to listen for a click of the start button
+
+// create a function to render the quiz
+
+// create a function to render a quiz timer (interval)
+
+// create a function to retain the quiz scores (local storage set)
+
+// create a function to render the high scores (local storage get)
