4eac4dd50b49f331f442ab9463128d81ea010d66
EowynStark <andwellm89@gmail.com>
2023-11-07 21:08:50 -0600

    added variables for quiz information

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,30 @@
 var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".countdown-timer");
 
-// create a function to listen for a click of the start button
-startButton.addEventListener("click", function()){
-    // call quiz function
-    // call timer function
-} 
+var question1 = "Primitive types of data include: undefined, numbers, boolean, string. True or False?";
+var question2 = "An array is a collection of manta rays. True or False?";
+var question3 = "'array.pop()' removes an item from an array. True or False?";
+var question4 = "You can call a function by using 'console.log(functionName)'. True or False?";
+var answer1 = "True";
+var answer2 = "False";
+var answer3 = "True";
+var answer4 = "False";
+
+
+// listen for a click of the start button to begin quiz
+startButton.addEventListener("click", startQuiz, timedQuiz);
+
 // create a function to render the quiz
-    
+function startQuiz(){
+   
+   
+}    
 
 // create a function to render a quiz timer (interval)
     // timer subtracts on a wrong answer
     // timer activates game over when time runs out or questions exhausted
 function timedQuiz (){
-    var timeLeft = 120;
+    var timeLeft = 1000 ;
     var timeCountdown = setInterval(function(){
         if (timeLeft > 1) {
             timerElement.textContent = timeleft + " seconds left";
