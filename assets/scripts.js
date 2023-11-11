bfee809b7546e99d7e584608f29911d8795d373f
EowynStark <andwellm89@gmail.com>
2023-11-11 11:17:14 -0600

    code for quiz functionality in progress

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,25 +1,89 @@
 var startButton = document.querySelector(".start-button");
-var timerElement = document.querySelector(".countdown-timer");
+var timerElement = document.getElementById('countdown-timer');
+var questionDisplay = document.getElementById('quiz-container');
+var answerDisplay = document.getElementById('answer-container');
+var scoreElement = document.getElementById('current-score');
+var scoreBoardElement = document.getElementById('past-score');
 
 
+var timeLeft = 120;
+var timeCountdown;
+var score = 0;
+
+var questionsIndex= 0;
+var questions = [
+    {
+        question: "From the following which is NOT a Primitive Data type?",
+        answers: ["boolean", "number", "string", "array"],
+        correctAnswer: "array",
+    },
+    {
+        question: "What do you call a variable that includes a list of items as its value?",
+        answers: ["array", "list", "function", "console"],
+        correctAnswer: "array",
+    },
+    {
+        question: "What method do you use to remove a specific list item from an array?",
+        answers: [".flat()", ".push()", ".pop()", ".concat()"],
+        correctAnswer: ".pop()",
+    },
+    {
+        question: "What data type returns only 'true' or 'false' as a value?",
+        answers: ["number", "boolean", "string", "undefined"],
+        correctAnswer: "boolean",
+    },
+];
 
 // listen for a click of the start button to begin quiz
-startButton.addEventListener("click", timedQuiz);
+startButton.addEventListener("click", startQuiz);
 
-// create a function to render the quiz
+// function to begin the quiz and timer
 function startQuiz(){
-   
-   
+   timedQuiz();
+   question1();
 }    
+// function to load first question
+function question1(){
+    var question1 = questions[questionsIndex];
+    questionDisplay.textContent = question1.question;
 
-// create a function to render a quiz timer (interval)
-    // timer subtracts on a wrong answer
-    // timer activates game over when time runs out or questions exhausted
+    question1.answers.forEach((choice) => {
+        var answerButton = document.createElement("button");
+        answerButton.textContent = question1.answers;
+        answerButton.addEventListener("click", () => checkAnswer(choice));
+        questionDisplay.appendChild(answerButton);
+    });
+}
+// create a function to check answer (detract from timer if wrong)
+function checkAnswer(userChoice) {
+    var question1 = questions[questionsIndex];
+    
+    if (userChoice === question1.correctAnswer){
+        score++;
+        scoreBoard();
+    }else {
+        timeLeft--;
+    }
+    question2();
+}
+
+// function moves onto next question in list
+function question2(){
+    questionsIndex++;
+    
+    if (questionsIndex < questions.length) {
+        question1();
+    } else {
+        clearInterval(timedQuiz);
+        timerElement.textContent = "GAME OVER";
+    }
+}
+
+// rendering a quiz timer
 function timedQuiz (){
-    var timeLeft = 1000 ;
-    var timeCountdown = setInterval(function(){
+    timeCountdown = setInterval(function(){
         if (timeLeft > 1) {
-            timerElement.textContent = timeleft + " seconds left";
+            timerElement.textContent = timeLeft + " seconds left";
             timeLeft--;
         } else if (timeLeft === 1){
             timerElement.textContent = timeLeft + " second left";
@@ -30,11 +94,17 @@ function timedQuiz (){
             // insert function call for scoreboard
         }
     },1000);
-}    
+}   
 // create a function to retain the quiz scores (local storage set)
+function scoreBoard(){
+    scoreElement.textContent = score;
+    localStorage.setItem(score)
+}
 
 // create a function to render the high scores (local storage get)
-
+function pastScores(){
+    scoreBoardElement.textContent = localStorage.getItem(score)
+}
 /* remove before publishing
 GIVEN I am taking a code quiz
 WHEN I click the start button
